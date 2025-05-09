4fd337c576dd1454c6103b88b23556377fdc9c46
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-09 20:15:20 +0300

    chore: Successfully connected database

---
+++ HCM app/Migrations/20250509171442_StartDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HCM_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class StartDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
