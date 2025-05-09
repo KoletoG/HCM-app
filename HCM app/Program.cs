c2e180fced6e7a1220451ca7e30837ed26011e40
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-09 20:12:01 +0300

    chore: Added SQL connection with EF

--- HCM app/Program.cs
+++ HCM app/Program.cs
@@ -1,3 +1,6 @@
+using HCM_app.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace HCM_app
 {
     public class Program
@@ -5,12 +8,11 @@ namespace HCM_app
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
