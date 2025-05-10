782dfa987561682ea751531723bb633f364436e7
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-10 09:22:52 +0300

    refactor: Moved UserDataModel to a shared class library

---
+++ SharedModels/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharedModels
+{
+    public class UserDataModel
+    {
+            [Key]
+            public string Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string JobTitle { get; set; }
+            public double Salary { get; set; }
+            public string Department { get; set; }
+            public string Password { get; set; }
+            public string PasswordHash { get; set; }
+            public UserRole Role { get; set; }
+            public UserDataModel()
+            {
+
+            }
+    }
+    public enum UserRole
+    {
+        Employee,
+        Manager,
+        HrAdmin
+    }
+}
