3a9e937d4510d2b333041c1877e410c95880e57a
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-10 09:17:55 +0300

    feat: Added ApplicationDbContext in CRUDHCM_API

---
+++ CRUDHCM_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDHCM_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
--- CRUDHCM_API/Program.cs
+++ CRUDHCM_API/Program.cs
@@ -1,4 +1,7 @@
 
+using CRUDHCM_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace CRUDHCM_API
 {
     public class Program
@@ -12,6 +15,7 @@ namespace CRUDHCM_API
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
