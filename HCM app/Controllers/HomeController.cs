fddb4c7aae974f008b83943bd6e5d60ab2fe2fd1
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-10 10:08:41 +0300

    feat: Added instances of HttpClient in HomeController

--- HCM app/Controllers/HomeController.cs
+++ HCM app/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using HCM_app.Migrations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HCM_app.Controllers
@@ -6,15 +7,22 @@ namespace HCM_app.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly HttpClient _clientAuth;
+        private readonly HttpClient _clientCRUD;
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory client)
         {
             _logger = logger;
+            _clientAuth = client.CreateClient("AuthAPI");
+            _clientCRUD = client.CreateClient("CRUDAPI");
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult GetUsers()
+        {
+            return View("Index");
         }
 
         public IActionResult Privacy()
--- HCM app/Program.cs
+++ HCM app/Program.cs
@@ -12,6 +12,15 @@ namespace HCM_app
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddHttpClient("AuthAPI", client =>
+                {
+                    client.BaseAddress = new Uri("https://localhost:7029/");
+                });
+
+            builder.Services.AddHttpClient("CRUDAPI", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:7261/");
+            });
             var app = builder.Build();
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
