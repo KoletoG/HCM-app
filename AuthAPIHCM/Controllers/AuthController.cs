900b1431610ad3d975e993ca97027698c0d6abfc
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-10 18:09:11 +0300

    feat: Added Register() and Login()

--- AuthAPIHCM/Controllers/AuthController.cs
+++ AuthAPIHCM/Controllers/AuthController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SharedModels;
 using AuthAPIHCM.Data;
+using AuthAPIHCM.Services;
+using AuthAPIHCM.Interfaces;
 
 namespace AuthAPIHCM.Controllers
 {
@@ -16,10 +18,37 @@ namespace AuthAPIHCM.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public AuthController(ApplicationDbContext context)
+        private readonly IAuthService _authService;
+        public AuthController(ApplicationDbContext context, IAuthService authService)
         {
             _context = context;
+            _authService = authService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserDataModel user)
+        {
+            if (string.IsNullOrEmpty(user.Password))
+            {
+                return BadRequest("Password cannot be empty.");
+            }
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Register), new { id = user.Id }, user);
         }
 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserDataModel login)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Email == login.Email);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(login.PasswordHash, user.PasswordHash))
+            {
+                return Unauthorized("Invalid credentials");
+            }
+
+            var token = _authService.GenerateJwtToken(user);
+            return Ok(new { Token = token });
+        }
     }
 }
