1284baa2d7a0b057671801e878768ad7ac7ae45b
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-10 09:53:11 +0300

    feat: Added GetAllUsers(), GetUserById() and AddUser(() in CRUDHCM_API

---
+++ CRUDHCM_API/Controllers/CRUDController.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using CRUDHCM_API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SharedModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRUDHCM_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CRUDController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CRUDController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<CRUDController>
+        [HttpGet]
+        public async Task<IEnumerable<UserDataModel>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET api/<CRUDController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(string id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user != default)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // POST api/<CRUDController>
+        [HttpPost]
+        public async Task<IActionResult> AddUser([FromBody] UserDataModel user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        // PUT api/<CRUDController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CRUDController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
