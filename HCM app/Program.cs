fbb87178ce8be8bc0b2b4b4b696640a13e04ea34
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-11 23:58:24 +0300

    feat: Added security measures against clickjacking and MIME sniffing

--- HCM app/Program.cs
+++ HCM app/Program.cs
@@ -59,6 +59,12 @@ namespace HCM_app
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Frame-Options", "DENY");
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                await next();
+            });
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseSession();
