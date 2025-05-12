fae662b44244745e0d21da1336a55f80b97320e5
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-12 08:02:51 +0300

    style: Profile view is now showing user's information

--- HCM app/ViewModels/ProfileViewModel.cs
+++ HCM app/ViewModels/ProfileViewModel.cs
@@ -1,21 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace HCM_app.ViewModels
 {
     public class ProfileViewModel
     {
         [Required]
+        [DisplayName("First name")]
         public string FirstName { get; set; }
         [Required]
+        [DisplayName("Last name")]
         public string LastName { get; set; }
         [Required]
         [EmailAddress]
+        [DisplayName("Email")]
         public string Email { get; set; }
+        [DisplayName("Job title")]
         public string JobTitle { get; set; }
         [DataType(DataType.Currency)]
+        [DisplayName("Salary")]
         public double Salary { get; set; }
+        [DisplayName("Department")]
         public string Department { get; set; }
+        [DisplayName("Role")]
         public string Role { get; set; }
     }
 }
