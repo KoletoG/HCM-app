16d2bca2e6192a96e9827ad17117c219af3e8bb3
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-16 10:10:36 +0300

    feat: Added new service TokenService

---
+++ HCM app/Services/TokenService.cs
@@ -0,0 +1,32 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace HCM_app.Services
+{
+    public class TokenService
+    {
+        private readonly JwtSecurityTokenHandler _handler;
+        public TokenService(JwtSecurityTokenHandler handler) 
+        {
+            _handler = handler;
+        }
+        public JwtSecurityToken GetToken(byte[] tokenBytes)
+        {
+            var tokenString = Encoding.UTF8.GetString(tokenBytes);
+            return _handler.ReadJwtToken(tokenString);
+        }
+        public string GetRole(JwtSecurityToken token)
+        {
+            return token.Claims.First(x => x.Type == ClaimTypes.Role).Value;
+        }
+        public string GetId(JwtSecurityToken token)
+        {
+            return token.Claims.First(x => x.Type == "sub").Value;
+        }
+        public string GetDepartment(JwtSecurityToken token)
+        {
+            return token.Claims.First(x => x.Type == "Department").Value;
+        }
+    }
+}
